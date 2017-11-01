a276d19ceaec69fb4a1cf19155aef666ec27a007
Juan Gallego IV <juan.gallego.iv@gmail.com>
2017-11-01 11:26:39 +0100

    Add implementation for types, structs, and schemas

--- apps/admin/lib/blog/admin/articles/form_schema.rb
+++ apps/admin/lib/blog/admin/articles/form_schema.rb
@@ -5,6 +5,9 @@ module Blog
   module Admin
     module Articles
       FormSchema = Dry::Validation.Form do
+        required(:title).filled
+        required(:status).filled(Types::ArticleStatus)
+        optional(:published_at).maybe(:time?)
       end
     end
   end
